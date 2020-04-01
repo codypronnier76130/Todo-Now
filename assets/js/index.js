f818c5c878bc1fe598132756698ae7bec237c32a
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-04-01 15:18:20 +0200

    Collapse de la navbar au changement de section

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,23 +5,78 @@ $(document).ready( function(){
     let navbar = $('#navbar');
     let accueil = $('#accueil');
     let connexion = $('#connexion');
+    let register = $('#register');
 
     // Affichage au démarrage du site
     $(navbar).show();
     $(accueil).show();
     $(connexion).hide();
+    $(register).hide();
 
 
-    //Boutons de login et register
+    //Boutons de login, register, et bouton d'accueil
+    let btnhome = $(navbar).find('.navbar-brand');
     let btnlogin = $('#btn-login');
     let btnregister = $('#btn-register');
+    let navbarcollapse = $(navbar).find('.navbar-collapse')
+
+    btnhome.click(function(){
+        $(accueil).show();
+        $(connexion).hide();
+        $(register).hide();
+
+
+
+
+
+        $(navbarcollapse).removeClass('collapse');
+        $(navbarcollapse).removeClass('show');
+        $(navbarcollapse).addClass('collapsing');
+        $(navbarcollapse).addClass('hide');
+    })
+
 
     btnlogin.click(function(){
 
         $(accueil).hide();
+        $(register).hide();
         $(connexion).show();
+
+
+
+
+        $(navbarcollapse).removeClass('collapse');
+        $(navbarcollapse).removeClass('show');
+        $(navbarcollapse).addClass('collapsing');
+        $(navbarcollapse).addClass('hide');
 
     });
 
+    btnregister.click(function(){
+
+        $(accueil).hide();
+        $(connexion).hide();
+        $(register).show();
+
+
+
+        
+        $(navbarcollapse).removeClass('collapse');
+        $(navbarcollapse).removeClass('show');
+        $(navbarcollapse).addClass('collapsing');
+        $(navbarcollapse).addClass('hide');
+    });
+
+
+
+
+
+
+
+
+
+
 
 });
+
+
